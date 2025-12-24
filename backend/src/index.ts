8c5d387d754b7b5e98a1a6050412c55b8350026b
Clement Delphin <clementcla7@gmail.com>
2025-12-24 19:14:40 +0530

    feat: add API route structure for auth and blog operations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.post('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('signup route!')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('signin route!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('adding blog route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('editing blog!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('getting particular blog!')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Getting all blogs!')
 })
 
 export default app
