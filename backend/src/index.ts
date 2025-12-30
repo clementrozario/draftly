c166c87cf040a5c8e18cf446705c9eb0db2847b5
Clement Delphin <clementcla7@gmail.com>
2025-12-30 18:55:02 +0530

    feat(auth): implement jwt middleware for protected routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,33 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign,verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
     JWT_SECRET: string
   }
+  Variables: {
+    userId: string;
+  }
 }>()
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  const jwt = c.req.header('Authorization');
+  if (!jwt) {
+    c.status(401);
+    return c.json({error:'unauthorized'})
+  }
+  const token = jwt.split(' ')[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({error: 'unauthorized'})
+  }
+  c.set('userId', payload.id as string);
+  await next()
+})
 
 app.post('/api/v1/user/signup', async (c) => {
   const prisma = new PrismaClient({
