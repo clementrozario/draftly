e0a0b170b09e430449e6a65a098f1e3f763ee924
Clement Delphin <clementcla7@gmail.com>
2025-12-31 20:22:25 +0530

    git commit -m "WIP: add basic blog post creation endpoint

---
+++ backend/routes/user.ts
@@ -0,0 +1,20 @@
+
+
+app.post('/', async (c) => {
+    const userId = c.get('userId');
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const post = await prisma.post.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            authorId: userId
+        }
+    });
+    return c.json({
+        id: post.id
+    });
+})
